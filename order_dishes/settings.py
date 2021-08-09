73b06652b5b76088a358021c0b831b0fa0df5f8a
SmurfikComunist <test.webserver.http@gmail.com>
2021-08-09 20:05:29 +0300

    feat: add debug-toolbar extension

--- order_dishes/settings.py
+++ order_dishes/settings.py
@@ -16,6 +16,10 @@ DEBUG = settings.DEBUG
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -24,10 +28,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'dish',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- order_dishes/urls.py
+++ order_dishes/urls.py
@@ -1,9 +1,17 @@
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+
+from order_dishes.config import settings
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('dish/', include('dish.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(
+        path('__debug__/', include(debug_toolbar.urls))
+    )
